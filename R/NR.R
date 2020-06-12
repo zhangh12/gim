66c11f3f87f6fa3cf1320deadb4e58e740a2c927
Han Zhang <zhangh.ustc@gmail.com>
2020-06-12 01:14:49 -0400

    v0.33.0
    remove temporary functions added in last version. Submitted to CRAN for
    paper that is under review at JRSSB. Tested the example in help doc
    seems like same results as previous version obtained.

--- R/NR.R
+++ R/NR.R
@@ -7,9 +7,8 @@ NR <- function(para, map, family, data, ref, V, bet0, sample.info, outcome, type
   para.null <- rep(NA, np)
   
   i <- 0
-  fn <- NULL
   while(i<1000){
-    fn <- c(fn, compute.obj(para, map, family, data, ref, inv.V, bet0, sample.info, outcome, type))
+    #fn <- compute.obj(para, map, family, data, ref, inv.V, bet0, sample.info, outcome, type)
     
     s0 <- compute.score(para, map, family, data, ref, inv.V, bet0, sample.info, outcome, type)
     
@@ -20,15 +19,12 @@ NR <- function(para, map, family, data, ref, V, bet0, sample.info, outcome, type
     }
     
     h0 <- compute.hess(para, map, family, data, ref, inv.V, bet0, sample.info, outcome, type)
-    #h0 <- check.neg.def(h0)
     
     t0 <- try(d0 <- solve(h0, s0), silent = TRUE)
     if('try-error' %in% class(t0)){
       stop('hess fails')
     }
     #d0 <- d0/sqrt(sum(p^2))
-    
-    #a <- wolfe.condition(para, map, family, data, ref, inv.V, bet0, sample.info, outcome, type, d0, s0, o0)
     
     para <- para - d0
     
@@ -39,8 +35,6 @@ NR <- function(para, map, family, data, ref, V, bet0, sample.info, outcome, type
   if(all(abs(s0) > 1e-6)){
     stop('NR does not converge')
   }
-  
-  plot(fn, pch = 20)
   
   list(coefficients = para, score = s0, conv = ifelse(all(abs(s0) < 1e-6), 1, 0))
   
